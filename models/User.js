d07e2bdcb06422fbb0006c4fffed220c0219f04a
alejandro <alejandroweil13@gmail.com>
2021-03-28 16:15:34 -0400

    In tweets backend

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UsersSchema = new Schema({
+    handle: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('users', UsersSchema);
+module.exports = User;
