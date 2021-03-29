f275d3dff375f9cc1368193e6ccb82c3bfeab13d
alejandro <alejandroweil13@gmail.com>
2021-03-28 22:44:08 -0400

    Finished twitter clone

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require("./models/User");
 const bodyParser = require("body-parser");
+const passport = require('passport');
 
 
 
@@ -19,16 +20,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
-app.get("/", (req, res) => {
-    const user = new User({
-        handle: "jim",
-        email: "jim@jim.jim",
-        password: "jimisgreat123"
-    })
-    user.save();
-    res.send("Hello World!");
-});
 
 app.use("/api/users", users)
 app.use("/api/tweets", tweets)
